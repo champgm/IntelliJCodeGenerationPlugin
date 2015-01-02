15b0f76bf314f194bf6eeecd23f7010c1658752d
Mac Champion <mac.champion@cerner.com>
2015-01-02 15:45:38 -0600

    08-Preconditions-Parameters-And-Fields

---
+++ src/com/champgm/intellij/plugin/PluginUtil.java
@@ -0,0 +1,23 @@
+package com.champgm.intellij.plugin;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+
+public class PluginUtil {
+    public static PsiClass getPsiClassFromContext(final AnActionEvent e) {
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        if (psiFile == null || editor == null) {
+            return null;
+        }
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement elementAt = psiFile.findElementAt(offset);
+        return PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
+    }
+}
