aef5d0a74a88a9f169e200bf795bf5b2f5a67bdd
Mac Champion <mac.champion@cerner.com>
2015-01-13 12:45:39 -0600

    14-Finalize-Constructor-Modified-Fields

--- src/com/champgm/intellij/plugin/variables/MakeVariablesFinal.java
+++ src/com/champgm/intellij/plugin/variables/MakeVariablesFinal.java
@@ -1,11 +1,14 @@
 package com.champgm.intellij.plugin.variables;
 
+import org.jetbrains.annotations.NotNull;
+
 import com.champgm.intellij.plugin.PluginUtil;
 import com.google.common.collect.ImmutableSet;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiCodeBlock;
 import com.intellij.psi.PsiDeclarationStatement;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiForeachStatement;
@@ -17,7 +20,7 @@ import com.intellij.psi.PsiVariable;
 
 public class MakeVariablesFinal extends AnAction {
     @Override
-    public void actionPerformed(final AnActionEvent actionEvent) {
+    public void actionPerformed(@NotNull final AnActionEvent actionEvent) {
         final PsiClass psiClass = PluginUtil.getPsiClassFromContext(actionEvent);
         new WriteCommandAction.Simple(psiClass.getProject(), psiClass.getContainingFile()) {
             @Override
@@ -29,43 +32,46 @@ public class MakeVariablesFinal extends AnAction {
 
     private void addNecessaryFinalModifiers(final PsiClass psiClass) {
         final PsiMethod[] methods = psiClass.getMethods();
-        ImmutableSet.Builder<PsiVariable> unmodifiedLocalVariables = ImmutableSet.builder();
-        for (PsiMethod method : methods) {
-            final PsiElement[] methodStatements = method.getBody().getChildren();
-            for (PsiElement statement : methodStatements) {
-                if(statement instanceof PsiDeclarationStatement){
-                    final PsiDeclarationStatement localVariableDeclaration = (PsiDeclarationStatement) statement;
-                    final PsiElement[] declarationPieces = localVariableDeclaration.getChildren();
-                    for (PsiElement psiElement : declarationPieces) {
-                        if(psiElement instanceof PsiLocalVariable){
-                            final PsiLocalVariable localVariable = (PsiLocalVariable) psiElement;
-                            if (!PluginUtil.isModified(localVariable)) {
-                                unmodifiedLocalVariables.add(localVariable);
+        final ImmutableSet.Builder<PsiVariable> unmodifiedLocalVariables = ImmutableSet.builder();
+        for (final PsiMethod method : methods) {
+            final PsiCodeBlock body = method.getBody();
+            if (body != null) {
+                final PsiElement[] methodStatements = body.getChildren();
+                for (PsiElement statement : methodStatements) {
+                    if (statement instanceof PsiDeclarationStatement) {
+                        final PsiDeclarationStatement localVariableDeclaration = (PsiDeclarationStatement) statement;
+                        final PsiElement[] declarationPieces = localVariableDeclaration.getChildren();
+                        for (PsiElement psiElement : declarationPieces) {
+                            if (psiElement instanceof PsiLocalVariable) {
+                                final PsiLocalVariable localVariable = (PsiLocalVariable) psiElement;
+                                if (!PluginUtil.isModified(localVariable)) {
+                                    unmodifiedLocalVariables.add(localVariable);
+                                }
                             }
                         }
-                    }
-                }else{
-                    if ( statement instanceof PsiForeachStatement) {
-                        final PsiForeachStatement foreach = (PsiForeachStatement) statement;
-                        final PsiParameter iterationParameter = foreach.getIterationParameter();
-                        if(!PluginUtil.isModified(iterationParameter)) {
-                            unmodifiedLocalVariables.add(iterationParameter);
+                    } else {
+                        if (statement instanceof PsiForeachStatement) {
+                            final PsiForeachStatement foreach = (PsiForeachStatement) statement;
+                            final PsiParameter iterationParameter = foreach.getIterationParameter();
+                            if (!PluginUtil.isModified(iterationParameter)) {
+                                unmodifiedLocalVariables.add(iterationParameter);
+                            }
                         }
                     }
                 }
             }
         }
-        for (PsiVariable variable : unmodifiedLocalVariables.build()) {
+        for (final PsiVariable variable : unmodifiedLocalVariables.build()) {
             final PsiModifierList modifierList = variable.getModifierList();
-            if(modifierList!=null){
+            if (modifierList != null) {
                 modifierList.setModifierProperty("final", true);
             }
         }
     }
 
     @Override
-    public void update(final AnActionEvent e) {
-        PsiClass psiClass = PluginUtil.getPsiClassFromContext(e);
+    public void update(@NotNull final AnActionEvent e) {
+        final PsiClass psiClass = PluginUtil.getPsiClassFromContext(e);
         e.getPresentation().setEnabled(psiClass != null);
     }
 }
